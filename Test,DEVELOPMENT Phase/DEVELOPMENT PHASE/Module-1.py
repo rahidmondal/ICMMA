ffb93e22c1be81b9c8296878446eb41f178f77b1
RAHID <rahid.2004.personal@gmail.com>
2022-01-04 23:11:44 +0530

    First Commit

---
+++ Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/Module-1.py
@@ -0,0 +1,11 @@
+import mysql.connector,time
+
+#This File will run for once when program runs for first time .
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234"
+    )
+
+
