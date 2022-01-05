9fd7e8edff36f71982cb89901fae121213e53354
RAHID <rahid.2004.personal@gmail.com>
2022-01-05 10:59:11 +0530

    COMIT 3 AT 05-6-2022 DONE WITH THE SKELITON OF app.py

--- Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/IR.py
+++ Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/IR.py
@@ -1,6 +1,7 @@
 #This File Will Contain All The Things which needs to be runn before the actual program 
 #This File Will Run only for the first time ..
 
+from logging import error
 import mysql.connector,time
 
 try : 
@@ -11,11 +12,21 @@ try :
         )
     print("Connection Succesfull With Host at",time.ctime())
     try:
-        '''
+        
         mycursor = mydb.cursor()
+        '''
         mycursor.execute("CREATE DATABASE SCD")
         print("Database SCD created")
         '''
+        try :
+            time.sleep(10)
+            mycursor.execute("USE SCD")
+            print("Creating Tables!")
+            mycursor.execute("CREATE TABLE (ID varchar(10) ,TEACHER (NAME VARCHAR(50),ClassTeaching varchar(50),Phone varchar(15),SubjectTeaching varchar(100),Email varchar(50),PRIMARY KEY(ID) );")
+
+            
+        except:
+            print(error)
         
     except:
         print("Error Creating Database M1-DB-A2")
--- Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/app.py
+++ Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/app.py
@@ -1,1 +1,79 @@
-import contactmng,Messege
+#LOGS
+#Program Initial HAS BEEN COMPLETED ON 05-01-2022
+
+import M1,M2,time
+print("Application Starting.......")
+time.sleep(2)
+
+
+def app():
+    print("----------------------------BULK MESSEGING SYSTEM !!!------------------------- ")
+    print("Welcome User !! ")
+    print("Login Time : ",time.ctime())
+    print("1.Contact Management  ")
+    print("2.Messege System      ")
+    print("3.LICENCE & CREDITS")
+    print("4.ABOUT THE APPLICATION+MANUEL")
+    print("5.Logout")
+    Ch = input("Please Enter Your Choice : ")
+    if Ch == '1':
+        pass
+    elif Ch == '2':
+        pass
+    elif Ch == '3':
+        print()
+    elif Ch == '4' :
+        print()
+    
+    elif Ch == '5':
+        print("Logging Out...............")
+        time.sleep(3)
+        print("Logout Succesfull At : ",time.ctime())
+        print("**************************************************************************")
+        time.sleep(3)
+    
+    else : 
+        print("Invalid Selection !!! ")
+        app()
+    
+
+
+
+
+
+
+
+
+def init():
+    print("------------------------------Welcome User !!!-----------------------------------")
+    Z = input("Please Enter USER_NAME :  ")
+
+    try :
+
+        if Z == 'PUSHPAM' :
+            X = input("Please Enter Password : ")
+            if X == 'RAHID' :
+                print("Login In.......")
+                time.sleep(2)
+                app()
+            else :
+                print("INCORRECT PASSWORD ")
+                init()
+
+        else:
+            print("Incorrect Username !!!")
+            init()
+
+            
+    except:
+        print("Something Went Wrong")
+        init()
+
+
+
+
+
+
+#Test Area Below !!!!!
+
+init()
