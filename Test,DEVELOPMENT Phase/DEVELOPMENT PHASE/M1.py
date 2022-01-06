9eb7077e2aa095b027495c3e4e56d4b2a1b703ff
RAHID <rahid.2004.personal@gmail.com>
2022-01-06 18:46:20 +0530

    No major update

--- Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/M1.py
+++ Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/M1.py
@@ -1,11 +1,20 @@
+"""
+Program Status : WORKING (ETC - 10-01-2022)
+Program Name : M1.py - Module 1 File  .
+Description : This File Will Contain Feautures which will help in Contact Management .
+Part Of : Contant Managemnt and Bulk Messege Sending Application 
+Author : 
+        1.RAHID MONDAL
+        2.PUSHPAM JHA 
+
+"""
+
 import mysql.connector,time
-
-#This File will run for once when program runs for first time .
-
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="1234"
+    password="1234",
+    database = 'SCD'
     )
 
 
