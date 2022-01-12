38f06e1c51392f3e25241ad450b0c461c1b70f8f
RAHID <rahid.2004.personal@gmail.com>
2022-01-12 18:19:10 +0530

    Changed Basic IR and Added something in M1 smb

--- Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/M1.py
+++ Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/M1.py
@@ -13,7 +13,7 @@ import mysql.connector
 import app
 
 
-def DBC():
+def DBC(X):
 
         try: 
                 time.sleep(10)
@@ -24,7 +24,21 @@ def DBC():
                 password="1234",
                 database = 'SCD'
                 )
+
                 print("Database Connected ..")
+                try :
+                        myc = mydb.cursor()
+                        myc.execute(X)
+                        mydb.commit()
+                        print("Record Added Succesfully !!")
+                        
+                except :
+                        mydb.rollback()
+                        print("Unssuccessfull Transaction .. ")
+
+                finally :
+                        mydb.close() 
+                
         except:
                 print("Error Connecting To Database ")
 
@@ -41,17 +55,28 @@ def Teacher():
         """)
         tch = int(input("Please Enter your Chocie  : "))
         if tch == 1:
-                pass
-        elif tch == 2 :
-                pass
-        elif tch == 3 :
-                pass
-        elif tch == 4 :
-                pass
-        elif tch == 5 :
-                pass
+                print(".........Teacher-Add-Form.................")
+                ID = input("Enter ID :  ")
+                F_NAME = input("Full Name : ")
+                C_Teacher = input("ClassTeacher Of : ")
+                PHONE_NO = input("Phone Number : ")
+                Subject_Teaching = input("Intials of Subject Teaching : ")
+                Email = input("Email : ")
+                stm = "insert into teacher values('{}','{}','{}','{}','{}','{}' )".format(ID,F_NAME,C_Teacher,PHONE_NO,Subject_Teaching,Email)
+                DBC(stm)
 
-def student():
+
+        elif tch == 2 :
+                print(".........Teacher-Edit-Form................")
+        elif tch == 3 :
+                print(".........Teacher-Delete-Form..............")
+        elif tch == 4 :
+                print(".........Teacher-view-Form.................")
+        elif tch == 5 :
+                CM()
+
+        
+def student(): #STUDNET ADD BUGGY 
         print("...........................Student-Contacts....................")
         print("""
         1.ADD
@@ -62,18 +87,24 @@ def student():
         """)
         tch = int(input("Please Enter your Chocie  : "))
         if tch == 1:
-                pass
-        elif tch == 2 :
-                pass
-        elif tch == 3 :
-                pass
-        elif tch == 4 :
-                pass
-        elif tch == 5 :
-                pass
+              print("...............Student-Add-Form...................")
+              ID = input("ID : ")
+              F_NAME = input("Full Name : ")
+              Class = input("Class : ")
+              PHONE_NO = input("Phone Number : ")
+              Parents_Number = input("Parents Number : ")
+              email = input("Email : ")
+              En = "insert into teacher values('{}','{}','{}','{}','{}','{}' )".format(ID,F_NAME,Class,PHONE_NO,Parents_Number,email)
+              DBC(En)
 
-        
-      
+        elif tch == 2 :
+                print(".............Student-Edit-Form..................")
+        elif tch == 3 :
+                print(".............Student-Delete-Form................")
+        elif tch == 4 :
+                print(".............Student-View-Form...................")
+        elif tch == 5 :
+               CM()
 
 def CM():
         print("--------------------------Contact Management-----------------------")
@@ -102,7 +133,8 @@ def CM():
                 CM()
 
 if __name__ == '__main__':
-        CM()
+        student()
+
 
 
 
