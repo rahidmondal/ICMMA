fb15010f9ba68742058bd7fe48073a5d765771b5
Pushpam Jha <pushpamjha13102004@gmail.com>
2022-02-28 09:39:11 +0530

    Update M3.py

--- Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/M3.py
+++ Test,DEVELOPMENT Phase/DEVELOPMENT PHASE/M3.py
@@ -1,6 +1,6 @@
 #Program Status : ETA 17/01/2022
-#Program Name : M2.py - Module 3 SEND MESSEGES MODULE   .
-#Description : This File Will Contain Feautures which will help in sending messeges .
+#Program Name : M2.py - Module 3 SEND MESSAGES MODULE   .
+#Description : This File Will Contain Features which will help in sending messages .
 
 try : 
     import webbrowser 
